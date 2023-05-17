6c34bad941a4276b3054da67ccecee612fc29702
canicemichael <canicemichael@gmail.com>
2023-05-17 07:34:21 +0100

    input by stdin

---
+++ regex.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# echo Hello, who am I talking to?
+
+# read varname
+
+# echo It\'s nice to meet you $varname
+
+
+# read -p 'Username: ' uservar
+# read -sp 'Password: ' passvar
+# echo
+# echo Thankyou $uservar we now have your login details $passvar
+
+echo What cars do you like?
+
+read car1 car2 car3
+
+echo Your first car was: $car1
+echo Your second car was: $car2
+echo Your third car was: $car3
---
+++ stdin.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo Here is a summary of the sales data:
+echo ===================================
+echo
+# cat the file representing STDIN, cut setting the delimiter to a space, fields 2 and 3 then sort the output
+cat /dev/stdin | cut -d' ' -f 2,3 | sort
